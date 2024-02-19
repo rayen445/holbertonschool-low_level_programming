a126af4b49208acf4545ced2de2a8d3dbcbc6ba0
rayen445 <nesstinebacc@gmail.com>
2024-02-19 20:40:06 +0100

    commit

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main- the main entry gateway to the program
+ *
+ * Return: Always 0(Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
