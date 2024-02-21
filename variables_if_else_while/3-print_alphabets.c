2aa2ad36fd06d9153639b53b3c31e611e4a3b0c8
rayen445 <nesstinebacc@gmail.com>
2024-02-21 18:41:48 +0100

    commit

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-/**
- * main - Prints the alphabet in lowercase.
- *
- * Return: Always 0.
- */
+
 int main(void)
 {
-char L;
+    char letter;
 
-	for (L = 'a'; L <= 'z'; L++)
-        putchar(L);
-	putchar('\n');
-	return (0);
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+    }
+
+    for (letter = 'A'; letter <= 'Z'; letter++)
+    {
+        putchar(letter);
+    }
+
+    putchar('\n');
+
+    return 0;
 }
