ae56cac8ad9870608af902620e06bc778648ee86
rayen445 <nesstinebacc@gmail.com>
2024-02-21 18:30:09 +0100

    commit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0,
+ *              followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    putchar('0');
+    putchar('1');
+    putchar('2');
+    putchar('3');
+    putchar('4');
+    putchar('5');
+    putchar('6');
+    putchar('7');
+    putchar('8');
+    putchar('9');
+    putchar('\n');
+
+    return (0);
+}
