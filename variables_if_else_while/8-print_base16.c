c059140f3296ce12ea788726da0e6563e7ea077e
rayen445 <nesstinebacc@gmail.com>
2024-02-21 18:54:17 +0100

    commit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+int main(void)
+{
+    char digit;
+
+    for (digit = '0'; digit <= '9'; digit++)
+    {
+        putchar(digit);
+    }
+
+    for (digit = 'a'; digit <= 'f'; digit++)
+    {
+        putchar(digit);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
