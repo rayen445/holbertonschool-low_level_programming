b7fde52894d76a9bad78f1fd83ae4055c19347ac
rayen445 <nesstinebacc@gmail.com>
2024-02-22 12:24:39 +0000

    commit

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,21 +1,7 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
- */
-void print_alphabet_x10(void)
-{
-    char letter;
-    int i;
+void print_alphabet_x10(void);
+int _putchar(char c);
 
-    for (i = 0; i < 10; i++)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-    }
-}
+#endif /* MAIN_H */
