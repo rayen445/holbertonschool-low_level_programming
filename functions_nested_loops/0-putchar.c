59547513dcf08820db4ea008a59da917afc2bc77
rayen445 <nesstinebacc@gmail.com>
2024-02-22 11:21:55 +0000

    commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void) {
-    printf("_putchar\n");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _putchar('\n');
+    return (0);
 }
