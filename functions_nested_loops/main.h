287d91db52d534a2688e4f1c3ebbcde0c276bbe0
rayen445 <nesstinebacc@gmail.com>
2024-02-22 14:24:04 +0000

    commit

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Other function prototypes */
-int print_last_digit(int n);
-int _putchar(char c); // Ensure _putchar prototype is also included
+int _putchar(char c);
 
 #endif /* MAIN_H */
