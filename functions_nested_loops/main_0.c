aa2272bfdaa20abe1a29ed1535c5bf42bed9f477
rayen445 <nesstinebacc@gmail.com>
2024-02-22 12:41:21 +0000

    commit

--- functions_nested_loops/main_0.c
+++ functions_nested_loops/main_0.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- * test_islower - test _islower function
+ * test_islower - Test _islower function.
  * Return: 0
  */
 int test_islower(void)
 {
-    int r;
-    int n;
+	int r;
+	int n;
 
-    n = 'a';
-    r = _islower(n);
-    printf("%c: %d\n", n, r);
-    n = 'H';
-    r = _islower(n);
-    printf("%c: %d\n", n, r);
-    n = 108;
-    r = _islower(n);
-    printf("%c: %d\n", n, r);
-    return (0);
+	n = 'a';
+	r = _islower(n);
+	printf("%c: %d\n", n, r);
+	n = 'H';
+	r = _islower(n);
+	printf("%c: %d\n", n, r);
+	n = 108;
+	r = _islower(n);
+	printf("%c: %d\n", n, r);
+	return (0);
 }
