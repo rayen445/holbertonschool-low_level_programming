8b885eb05d1384fedb1c2a023fe541d4a45a79e9
rayen445 <nesstinebacc@gmail.com>
2024-02-24 09:59:54 +0000

    commit

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
-  * print_last_digit - Prints the last digit of a number
-  * @n: The number to be treated
-  *
-  * Return: Value of the last digit of number
-  */
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to be treated
+ *
+ * Return: Value of the last digit of number
+ */
 int print_last_digit(int n)
 {
-int last;
+    int last;
 
-last = n % 10;
+    last = n % 10;
 
-if (last < 0)
-{
-last = last * -1;
-}
-_putchar(last + '0');
-return (last);
+    if (last < 0)
+    {
+        last = last * -1;
+    }
+    _putchar(last + '0');
+    return (last);
 }
