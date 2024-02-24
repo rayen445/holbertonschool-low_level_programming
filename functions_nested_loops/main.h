e43e71177a654a4bab69d3594881df7d88378c60
rayen445 <nesstinebacc@gmail.com>
2024-02-24 10:06:04 +0000

    commit

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,22 +1,7 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * jack_bauer - prints every minute of the day of Jack Bauer
- */
-void jack_bauer(void)
-{
-    int hour, minute;
+/* Function prototype */
+int _putchar(char c);
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar('0' + (hour / 10)); /* Print tens digit of hour */
-            _putchar('0' + (hour % 10)); /* Print ones digit of hour */
-            _putchar(':');
-            _putchar('0' + (minute / 10)); /* Print tens digit of minute */
-            _putchar('0' + (minute % 10)); /* Print ones digit of minute */
-            _putchar('\n');
-        }
-    }
-}
+#endif /* MAIN_H */
