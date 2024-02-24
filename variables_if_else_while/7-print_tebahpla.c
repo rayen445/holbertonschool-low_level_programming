956fff67e3135ab79b414875f8d7aaf51d0e0410
rayen445 <nesstinebacc@gmail.com>
2024-02-24 08:01:11 +0000

    commit

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
-/**
- * main - Prints the lowercase alphabet in reverse.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-char letter;
+#include "main.h"
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
-	putchar('\n');
-	return (0);
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to extract the last digit from
+ *
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n)
+{
+    int last_digit = n % 10;
+
+    if (last_digit < 0)
+        last_digit *= -1;
+
+    _putchar(last_digit + '0');
+
+    return last_digit;
 }
