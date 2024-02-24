9bb6e40ff6ab95997e274a9bbd5e94f04ac62dd7
rayen445 <nesstinebacc@gmail.com>
2024-02-24 08:05:31 +0000

    commit

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * print_last_digit - Prints the last digit of a number
  * @n: The number to extract the last digit from
  *
- * Return: The value of the last digit
+ * Return: Value of the last digit
  */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+	int last_digit;
 
-    if (last_digit < 0)
-        last_digit *= -1;
-
-    _putchar(last_digit + '0');
-
-    return last_digit;
+	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
