50936fa9a031827aaa6eb1e4a5f61b88cf9fa68c
rayen445 <nesstinebacc@gmail.com>
2024-02-24 08:04:03 +0000

    commit

--- variables_if_else_while/main.c
+++ variables_if_else_while/main.c
@@ -1,8 +1,30 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
-int print_last_digit(int);
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to extract the last digit from
+ *
+ * Return: Value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
 
-#endif /* MAIN_H */y
+	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
