f612f204ebc2f49cd4394dc9e3e53af40941642b
rayen445 <nesstinebacc@gmail.com>
2024-02-24 10:54:30 +0000

    commit

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <limits.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
 
-int _putchar(char c);
-int print_last_digit(int n);
-
-#endif /* MAIN_H */
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
