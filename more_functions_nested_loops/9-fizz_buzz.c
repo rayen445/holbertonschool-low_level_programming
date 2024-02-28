9cfa0dcaf50074a817d682d9e2c645f05cf5b9f1
rayen445 <nesstinebacc@gmail.com>
2024-02-28 20:14:14 +0000

    commit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,51 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - Entry point, prints FizzBuzz sequence
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if ((i % 3 == 0) && (i % 5 == 0))
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", i);
-        }
-
-        if (i < 100)
-        {
-            printf(" ");
-        }
-        else
-        {
-            printf("\n");
-        }
-    }
-
-    return (0);
-}
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+		}
+		else if (i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+		if (i < 100)
+		{
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	retur
