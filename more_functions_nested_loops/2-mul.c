0d4663ef0604e66d57d8481c65fb4edbe8217817
rayen445 <nesstinebacc@gmail.com>
2024-02-28 18:20:11 +0000

    commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of multiplication
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
