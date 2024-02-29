5a56288f90e6237b1d33c8f7ace11eeea287d78f
rayen445 <nesstinebacc@gmail.com>
2024-02-29 12:07:30 +0000

    commit

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,6 @@
-#include "main.h" // Include the header file containing the prototype for the mul function
+#include "main.h"
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
     int i = 98;
