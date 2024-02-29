bc677892da0dd8587bf550b788d1301f12cec827
rayen445 <nesstinebacc@gmail.com>
2024-02-29 19:58:28 +0100

    commit

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,22 +6,22 @@
  */
 void print_triangle(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-            _putchar(' ');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+			_putchar('	');
 
-        for (j = 0; j <= i; j++)
-            _putchar('#');
+		for (j = 0; j <= i; j++)
+			_putchar('#');
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
