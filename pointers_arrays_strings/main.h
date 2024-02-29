df30db70ead28ee820fc55c9c152b0bc51c31e40
rayen445 <nesstinebacc@gmail.com>
2024-02-29 14:35:44 +0100

    git push

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,16 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+#include <unistd.h>
 
-void _putchar(char c);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+int _putchar(char c);
 
 #endif /* MAIN_H */
