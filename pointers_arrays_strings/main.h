fdfbff75b229f7d77c2a1d005f79c99c7be20763
rayen445 <nesstinebacc@gmail.com>
2024-03-01 00:35:46 +0100

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <limits.h>
+#include <stdio.h>
 
-int _atoi(char *s);
-void _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
