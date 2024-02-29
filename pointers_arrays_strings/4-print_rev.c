dfd2a0889a292ee8975acc978df89e97d6816786
rayen445 <nesstinebacc@gmail.com>
2024-02-29 14:22:03 +0100

    commit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse, followed by a new line
+ * @s: The string to be printed
+ */
+void print_rev(char *s)
+{
+    int length = 0;
+    int i;
+
+    // Calculate the length of the string
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    // Print the string in reverse
+    for (i = length - 1; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+
+    // Print a new line
+    _putchar('\n');
+}
