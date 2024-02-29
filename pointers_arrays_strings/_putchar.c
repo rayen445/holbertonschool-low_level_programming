a0bb908e2a7c718289148d8c3f8ebd06eaf52a97
rayen445 <nesstinebacc@gmail.com>
2024-02-29 14:41:06 +0100

    commit

--- pointers_arrays_strings/_putchar.c
+++ pointers_arrays_strings/_putchar.c
@@ -1,17 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * puts2 - Prints every other character of a string, starting with the first
- * @str: The string to print
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ *
+ * Return: Upon success, returns the character written.
+ *         On error, returns -1 and sets errno appropriately.
  */
-void puts2(char *str)
+int _putchar(char c)
 {
-    int i = 0;
-
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i += 2;
-    }
-    _putchar('\n');
+    return (write(1, &c, 1));
 }
