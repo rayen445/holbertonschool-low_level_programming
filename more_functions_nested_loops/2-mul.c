d259a1a975bd4a814d269ae476148198cadeee5e
rayen445 <nesstinebacc@gmail.com>
2024-02-29 11:58:02 +0000

    commit

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,9 @@
-#include "main.h"
-
 /**
  * mul - Multiplies two integers.
  * @a: The first integer.
  * @b: The second integer.
  *
- * Return: The result of multiplication.
+ * Return: The result of the multiplication.
  */
 int mul(int a, int b)
 {
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 int _putchar(char c);
-void print_triangle(int size);
+int mul(int a, int b);
 
 #endif /* MAIN_H */
