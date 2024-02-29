bc1bd9cf808fd17853282d0ab85867ea57e07128
rayen445 <nesstinebacc@gmail.com>
2024-02-29 20:02:32 +0100

    commit

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,6 +3,10 @@
 /**
  * print_triangle - prints a triangle of '#' characters
  * @size: size of the triangle
+ *
+ * Description: This function prints a triangle made of '#' characters.
+ *              The triangle's height is determined by the size parameter.
+ *              If size is 0 or negative, it prints only a new line.
  */
 void print_triangle(int size)
 {
@@ -17,11 +21,22 @@ void print_triangle(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
-			_putchar('	');
+			_putchar(' ');
 
 		for (j = 0; j <= i; j++)
 			_putchar('#');
 
 		_putchar('\n');
 	}
+}
+
+int main(void)
+{
+	print_triangle(0);
+	print_triangle(-10);
+	print_triangle(3);
+	print_triangle(9);
+	print_triangle(98);
+
+	return (0);
 }
