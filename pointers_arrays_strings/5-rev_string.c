70087086996a6fc34bdec807f3276c55fb186810
rayen445 <nesstinebacc@gmail.com>
2024-02-29 18:02:43 +0100

    commit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,19 @@
  */
 void rev_string(char *s)
 {
-    int i, j;
-    char temp;
+	int i, j;
+	char temp;
 
-    /* Find the length of the string */
-    for (i = 0; s[i] != '\0'; i++)
-        ;
+	/* Find the length of the string */
+	for (i = 0; s[i] != '\0'; i++)
+		;
 
-    /* i now holds the length of the string */
-    /* Reverse the string */
-    for (j = 0; j < i / 2; j++)
-    {
-        temp = s[j];
-        s[j] = s[i - j - 1];
-        s[i - j - 1] = temp;
-    }
+	/* i now holds the length of the string */
+	/* Reverse the string */
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = temp;
+	}
 }
