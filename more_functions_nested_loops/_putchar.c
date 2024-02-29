16d79397806a750467e555546edaa3f19d5bef3a
rayen445 <nesstinebacc@gmail.com>
2024-02-29 12:27:26 +0000

    commit

--- more_functions_nested_loops/_putchar.c
+++ more_functions_nested_loops/_putchar.c
@@ -4,10 +4,9 @@
  * _putchar - Writes a character to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success, 1. On error, -1 is returned
  */
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+    return write(1, &c, 1);
 }
