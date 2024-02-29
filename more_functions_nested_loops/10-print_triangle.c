070431fbb2bf65a13fbbbd1b2aef17b7815157e1
rayen445 <nesstinebacc@gmail.com>
2024-02-29 19:54:42 +0100

    commit

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,29 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_triangle - Prints a triangle of a given size.
- * @size: The size of the triangle to print.
+ * print_triangle - Function to print a triangle of hashtags.
+ * @size: Size of the triangle.
  */
 void print_triangle(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            _putchar(' ');
-        }
-        for (j = 0; j <= i; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
 }
