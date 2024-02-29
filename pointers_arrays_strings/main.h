a8ed55ce28c40ed8c232c152de9a122861135472
rayen445 <nesstinebacc@gmail.com>
2024-03-01 00:50:01 +0100

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,14 +3,7 @@
 
 #include <stdio.h>
 
-/* Function prototypes */
-int _putchar(char c);
-char *_strcat(char *dest, const char *src);
-char *_strncat(char *dest, const char *src, int n);
-int _strcmp(const char *s1, const char *s2);
-int _strncmp(const char *s1, const char *s2, size_t n);
-char *_strncpy(char *dest, const char *src, int n);
 void reverse_array(int *a, int n);
-void print_number(int n);
+void print_array(int *a, int n);
 
 #endif /* MAIN_H */
