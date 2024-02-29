855a1e21f66dacd6826a1697b16885113138453b
rayen445 <nesstinebacc@gmail.com>
2024-03-01 00:44:57 +0100

    commit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,9 @@
  */
 int _strcmp(const char *s1, const char *s2)
 {
-    int i;
+	int i;
 
-    for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++)
-        ;
-    return s1[i] - s2[i];
+	for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++)
+		;
+	return (s1[i] - s2[i]);
 }
