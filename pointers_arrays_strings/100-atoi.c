9c34235455b54c39f926e2b8bded8a2689932340
rayen445 <nesstinebacc@gmail.com>
2024-02-29 18:19:11 +0100

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,6 +11,7 @@ int _atoi(char *s)
     int result = 0;
     int sign = 1;
     int i = 0;
+    int num_started = 0;
 
     /* Skip whitespace characters */
     while (s[i] == ' ')
@@ -24,15 +25,20 @@ int _atoi(char *s)
     }
 
     /* Convert digits to integer */
-    while (s[i] >= '0' && s[i] <= '9') {
-        /* Check for integer overflow */
-        if (result > (INT_MAX / 10) || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)) {
-            if (sign == 1)
-                return INT_MAX;
-            else
-                return INT_MIN;
+    while (s[i] != '\0') {
+        if (s[i] >= '0' && s[i] <= '9') {
+            /* Check for integer overflow */
+            if (result > (INT_MAX / 10) || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)) {
+                if (sign == 1)
+                    return INT_MAX;
+                else
+                    return INT_MIN;
+            }
+            result = result * 10 + (s[i] - '0');
+            num_started = 1;
+        } else if (num_started) {
+            break;  // Exit loop if a non-digit character is encountered after the number has started
         }
-        result = result * 10 + (s[i] - '0');
         i++;
     }
 
