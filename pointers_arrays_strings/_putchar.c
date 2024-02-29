9dcabc4b8495db2e82d361a3f2908cc158e9f505
rayen445 <nesstinebacc@gmail.com>
2024-03-01 00:42:11 +0100

    commit

--- pointers_arrays_strings/_putchar.c
+++ pointers_arrays_strings/_putchar.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to write
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: On success 1, on error -1
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void _putchar(char c)
+int _putchar(char c)
 {
-    write(1, &c, 1);
+    return write(1, &c, 1);
 }
