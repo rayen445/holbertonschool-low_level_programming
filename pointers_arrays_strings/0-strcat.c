0a304df68fc5af374019cc8eecea9c50a0031c04
rayen445 <nesstinebacc@gmail.com>
2024-03-01 00:25:46 +0100

    commit

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	/* Calculate the length of the destination string */
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	/* Append the src string to the dest string */
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	/* Add the null terminator to the concatenated string */
+	dest[dest_len + i] = '\0';
+
+	return dest;
+}
