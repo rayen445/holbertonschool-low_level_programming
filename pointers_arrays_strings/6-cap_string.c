0d9d8b6f0be8da0d7974305d3989dba8b6b47afd
rayen445 <nesstinebacc@gmail.com>
2024-03-01 01:02:34 +0100

    commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,20 +8,23 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-            str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
-            str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-            str[i - 1] == '{' || str[i - 1] == '}')
-        {
-            if (str[i] >= 'a' && str[i] <= 'z')
-                str[i] -= 32;
-        }
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' || str[i - 1] == ',' ||
+		    str[i - 1] == ';' || str[i - 1] == '.' ||
+		    str[i - 1] == '!' || str[i - 1] == '?' ||
+		    str[i - 1] == '"' || str[i - 1] == '(' ||
+		    str[i - 1] == ')' || str[i - 1] == '{' ||
+		    str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		i++;
+	}
 
-    return str;
+	return str;
 }
