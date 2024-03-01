9fad2382ab340b4171b4f4cf7e441cf6a8096d52
rayen445 <nesstinebacc@gmail.com>
2024-03-01 20:18:07 +0100

    commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,29 +5,29 @@
  */
 void times_table(void)
 {
-    int row, column, result;
+	int row, column, result;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            result = row * column;
-            if (column == 0)
-                _putchar('0');
-            else
-            {
-                if (result <= 9)
-                    _putchar(' ');
-                else
-                    _putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-            if (column < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			result = row * column;
+			if (column == 0)
+				_putchar('0');
+			else
+			{
+				if (result <= 9)
+					_putchar(' ');
+				else
+					_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			if (column < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
