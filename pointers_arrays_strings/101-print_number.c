ee20318b1dfacb868dc897f3646520be51d05f1f
rayen445 <nesstinebacc@gmail.com>
2024-03-01 01:17:01 +0100

    commit

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -7,22 +7,22 @@
  */
 void print_number(int n)
 {
-    if (n == INT_MIN)
-    {
-        _putchar('-');
-        _putchar('2');
-        print_number(147483648); /* Handle the rest as positive number */
-        return;
-    }
+	if (n == INT_MIN)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648); /* Handle the rest as positive number */
+		return;
+	}
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    if (n / 10 != 0)
-        print_number(n / 10);
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-    _putchar((n % 10) + '0');
+	_putchar((n % 10) + '0');
 }
