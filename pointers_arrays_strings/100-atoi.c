99a7008ec700bc2e3eee26086415b31ae3e00e11
rayen445 <nesstinebacc@gmail.com>
2024-03-01 20:22:37 +0100

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,37 +4,28 @@
  * _atoi - Converts a string to an integer
  * @s: The string to convert
  *
- * Return: The converted integer
+ * Return: The integer value of the converted string
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-    /* Skip whitespace characters */
-    while (s[i] == ' ')
-        i++;
+	/* Skip all leading non-digit characters */
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
-    /* Check for sign character */
-    while (s[i] == '-' || s[i] == '+') {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
+	/* Convert the number */
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+	{
+		result = (result * 10) + (s[i] - '0');
+		i++;
+	}
 
-    /* Convert digits to integer */
-    while (s[i] >= '0' && s[i] <= '9') {
-        /* Check for integer overflow */
-        if (result > (INT_MAX / 10) || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)) {
-            if (sign == 1)
-                return INT_MAX;
-            else
-                return INT_MIN;
-        }
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
-
-    return sign * result;
+	return (result * sign);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h> /* Include necessary standard library headers */
+#include <stdio.h>
 
 int _putchar(char c);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
