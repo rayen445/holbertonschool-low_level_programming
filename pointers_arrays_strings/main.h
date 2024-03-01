ca3d444255233d5181da56feffb9ace49b5a642f
rayen445 <nesstinebacc@gmail.com>
2024-03-01 18:00:07 +0100

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h> // Include necessary standard library headers
+#include <unistd.h> /* Include necessary standard library headers */
 
-/* Function prototypes */
-void puts2(char *str);
-char *rot13(char *s);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+int _putchar(char c);
 
 #endif /* MAIN_H */
