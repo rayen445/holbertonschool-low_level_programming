605dbab70ff627d36a12084c57996416e368b975
rayen445 <nesstinebacc@gmail.com>
2024-03-01 18:09:38 +0100

    commit

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,15 +3,20 @@
 /**
  * puts2 - Prints every other character of a string, starting with the first
  * @str: The string to print
+ *
+ * Description: This function prints every other character of the string `str`,
+ * starting with the first character.
  */
 void puts2(char *str)
 {
     int i = 0;
 
+    /* Loop through the string and print every other character */
     while (str[i] != '\0')
     {
         _putchar(str[i]);
         i += 2; /* Skip every other character */
     }
+
     _putchar('\n');
 }
