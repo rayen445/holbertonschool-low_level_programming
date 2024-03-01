27b649db3e380fee4c7bcdc1c84e34dd83b69290
rayen445 <nesstinebacc@gmail.com>
2024-03-01 20:26:38 +0100

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,24 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+    int result = 0;
+    int sign = 1;
+    int i = 0;
 
-	/* Skip all leading non-digit characters */
-	while (s[i] && (s[i] < '0' || s[i] > '9'))
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
-	}
+    /* Skip all leading non-digit characters */
+    while (s[i] && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	/* Convert the number */
-	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
-	{
-		result = (result * 10) + (s[i] - '0');
-		i++;
-	}
+    /* Convert the number */
+    while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+    {
+        result = (result * 10) + (s[i] - '0');
+        i++;
+    }
 
-	return (result * sign);
+    return (result * sign);
 }
