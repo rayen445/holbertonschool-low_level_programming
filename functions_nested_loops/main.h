ca372935a41a6240478513aac406dfad00252df3
rayen445 <nesstinebacc@gmail.com>
2024-03-01 20:08:47 +0100

    commit

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,18 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+/*
+ * File: main.h
+ * Author: Your Name
+ * Desc: Header file containing prototypes for all functions
+ *       written in the main.c and referenced by the
+ *       0x03-more_functions_nested_loops directory.
+ */
 
-void _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+int _putchar(char c);
 void jack_bauer(void);
-void times_table(void);
-void print_to_98(int n);
 
 #endif /* MAIN_H */
