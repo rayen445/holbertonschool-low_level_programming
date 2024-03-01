8162c38cc509a683c1630c74c14d823440be0980
rayen445 <nesstinebacc@gmail.com>
2024-03-01 18:08:16 +0100

    commit

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    char *str = "Lorem ipsum...";
-
-    puts2(str);
-    return (0);
-}
-
-/**
  * puts2 - Prints every other character of a string, starting with the first
  * @str: The string to print
  */
