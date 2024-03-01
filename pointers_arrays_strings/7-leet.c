93cb8d73d15d402af01c37eaab515a522e1f16f4
rayen445 <nesstinebacc@gmail.com>
2024-03-01 01:06:06 +0100

    commit

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @str: The string to encode
+ *
+ * Return: A pointer to the encoded string
+ */
+char *leet(char *str)
+{
+    char *ptr = str;
+    char *leet_map = "aAeEoOtTlL";
+    char *leet_replace = "4433007711";
+
+    while (*ptr != '\0')
+    {
+        int i = 0;
+        while (leet_map[i] != '\0')
+        {
+            if (*ptr == leet_map[i])
+                *ptr = leet_replace[i];
+            i++;
+        }
+        ptr++;
+    }
+
+    return str;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *cap_string(char *str);
+char *leet(char *str);
 
 #endif /* MAIN_H */
