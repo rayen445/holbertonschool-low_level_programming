3dac1e5b0c20c48bd1f441ed683ab229000a099b
rayen445 <nesstinebacc@gmail.com>
2024-03-01 18:02:12 +0100

    commit

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string.
- * @str: The string to print.
+ * puts2 - Prints every other character of a string, starting with the first
+ * @str: The string to print
  */
 void puts2(char *str)
 {
@@ -11,7 +11,7 @@ void puts2(char *str)
     while (str[i] != '\0')
     {
         _putchar(str[i]);
-        i += 2;
+        i += 2; // Skip every other character
     }
     _putchar('\n');
 }
