f4c5dd713d5453ef8fb6071acfc3114140784e98
rayen445 <nesstinebacc@gmail.com>
2024-03-04 18:34:36 +0100

    commit

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[] __attribute__((unused))) {
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    /* Unused variable (void cast to prevent compiler warning) */
+    (void)argc;
+
     printf("%d\n", argc - 1);
-    return 0;
+    return (0);
 }
