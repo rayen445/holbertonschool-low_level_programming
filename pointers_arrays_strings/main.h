4f1c2f177ae5f01e2aee0b3801fdf14b3fb4bd7a
rayen445 <nesstinebacc@gmail.com>
2024-03-04 10:43:21 +0100

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,24 +1,6 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _strchr - locates a character in a string
- * @s: the string to search
- * @c: the character to locate
- *
- * Return: pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found
- */
-char *_strchr(char *s, char c)
-{
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return s;
-        s++;
-    }
+char *_strchr(char *s, char c);
 
-    if (c == '\0')
-        return s;
-
-    return NULL;
-}
+#endif /* MAIN_H */
