c4b03562c66034cd4a3ba998afe052c9c222a2d9
rayen445 <nesstinebacc@gmail.com>
2024-03-04 12:12:08 +0100

    commit

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 
-int main(void)
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to search
+ * @accept: the string containing the characters to match
+ *
+ * Return: the number of bytes in the initial segment of s which consist
+ * only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
 {
-    char *s = "First, solve the problem. Then, write the code.";
-    char *accept = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    unsigned int result = _strspn(s, accept);
+	unsigned int count = 0;
+	int match;
 
-    printf("%u\n", result);
-    return 0;
+	while (*s)
+	{
+		match = 0;
+		while (*accept)
+		{
+			if (*s == *accept)
+			{
+				match = 1;
+				count++;
+				break;
+			}
+			accept++;
+		}
+		if (match == 0)
+			break;
+		s++;
+	}
+	return count;
 }
