00dcdf70ebcdbe9368630a29c2e7aaa62a4ea459
rayen445 <nesstinebacc@gmail.com>
2024-03-04 16:03:09 +0100

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
