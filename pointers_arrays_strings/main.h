4b5cbe595248d7377b6249e90045ed9eac28473b
rayen445 <nesstinebacc@gmail.com>
2024-03-04 16:06:56 +0100

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
