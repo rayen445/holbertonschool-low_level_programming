254fc422d185f8bd7ef0168ae58fd5576010a40c
rayen445 <nesstinebacc@gmail.com>
2024-03-04 17:18:58 +0100

    commit

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse
- * @s: The string to print
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string to be printed
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s)
-    {
-        _print_rev_recursion(s + 1);
-        _putchar(*s);
-    }
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
