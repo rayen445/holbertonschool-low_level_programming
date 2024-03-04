0ee2c2adf1313357e623a96fb51f4f967bb8bc28
rayen445 <nesstinebacc@gmail.com>
2024-03-04 18:19:52 +0100

    commit

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,10 +3,12 @@
 /**
  * main - prints the number of arguments passed to the program
  * @argc: number of arguments
+ * @argv: array of arguments
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
+    (void)argv; /* Prevent unused parameter warning */
     _putcher(argc + '0');
     _putcher('\n');
 
