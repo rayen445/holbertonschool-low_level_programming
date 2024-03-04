87907789fc300d16d6db2673387c9e2852a6c906
rayen445 <nesstinebacc@gmail.com>
2024-03-04 17:48:26 +0100

    commit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,30 +1,32 @@
+/* 6-is_prime_number.c */
+
 #include "main.h"
+
+/**
+ * is_prime_helper - checks if a number is prime
+ * @n: the number to check
+ * @divisor: the divisor to check
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+int is_prime_helper(int n, int divisor)
+{
+    if (n <= 1)
+        return (0);
+    if (divisor > n / 2)
+        return (1);
+    if (n % divisor == 0)
+        return (0);
+    return (is_prime_helper(n, divisor + 1));
+}
 
 /**
  * is_prime_number - checks if a number is prime
  * @n: the number to check
  *
- * Return: 1 if n is prime, 0 otherwise
+ * Return: 1 if the number is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (is_prime_helper(n, 2));
-}
-
-/**
- * is_prime_helper - helper function to check if a number is prime
- * @n: the number to check
- * @i: the current divisor being checked
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime_helper(int n, int i)
-{
-	if (i >= n)
-		return (1);
-	if (n % i == 0)
-		return (0);
-	return (is_prime_helper(n, i + 1));
+    return (is_prime_helper(n, 2));
 }
--- recursion/main.h
+++ recursion/main.h
@@ -1,3 +1,5 @@
+/* main.h */
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -11,5 +13,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_prime_helper(int n, int divisor);
 
 #endif /* MAIN_H */
