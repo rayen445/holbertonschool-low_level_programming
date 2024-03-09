95003b039410d81abd344c0ccbac13bc19de8027
rayen445 <nesstinebacc@gmail.com>
2024-03-09 10:22:58 +0100

    commit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the program command-line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+    int result;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return 1;
+    }
+
+    result = atoi(argv[1]) * atoi(argv[2]);
+    printf("%d\n", result);
+
+    return 0;
+}
