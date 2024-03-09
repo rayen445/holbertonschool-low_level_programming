abddabe799d599b43243ff4583eac7d0c7409e8a
rayen445 <nesstinebacc@gmail.com>
2024-03-09 10:40:25 +0100

    commit

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,16 +10,16 @@
  */
 int main(int argc, char *argv[])
 {
-    int result;
+	int result;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    result = atoi(argv[1]) * atoi(argv[2]);
-    printf("%d\n", result);
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
 
-    return 0;
+	return (0);
 }
