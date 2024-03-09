015350cc7cd5ee0a8f6613d20f1922705f42271b
rayen445 <nesstinebacc@gmail.com>
2024-03-09 09:14:04 +0100

    commit

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,12 +1,26 @@
+/*
+ * File: 2-args.c
+ * Auth: Julien Barbier
+ * Desc: A program that prints all arguments it receives.
+ */
+
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the program command-line arguments
+ *
+ * Return: 0 if successful, otherwise a non-zero value
+ */
 int main(int argc, char *argv[])
 {
     int i;
 
-    for (i = 0; i < argc; i++) {
+    for (i = 0; i < argc; i++)
+    {
         printf("%s\n", argv[i]);
     }
 
-    return 0;
+    return (0);
 }
