4a2699c004ea0bb08b94db81bdf76702e4bb6650
rayen445 <nesstinebacc@gmail.com>
2024-03-09 09:33:24 +0100

    commit

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,11 +3,10 @@
 /**
  * main - Entry point
  * @argc: The number of command-line arguments
- * @argv: An array containing the program command-line arguments
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc)
 {
     printf("%d\n", argc - 1);
     return 0;
