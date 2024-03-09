44ef0fa32975df65edf5a6f7c220f4041dcee3c4
rayen445 <nesstinebacc@gmail.com>
2024-03-09 11:05:49 +0100

    commit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,28 +11,28 @@
  */
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-    int i, j;
+	int sum = 0;
+	int i, j;
 
-    if (argc < 2)
-    {
-        printf("0\n");
-        return 0;
-    }
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return 1;
-            }
-        }
-        sum += atoi(argv[i]);
-    }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
 
-    printf("%d\n", sum);
-    return 0;
+	printf("%d\n", sum);
+	return (0);
 }
