a1c9a6a4413e226867740514111a43eb4525e926
rayen445 <nesstinebacc@gmail.com>
2024-03-12 14:43:14 +0100

    commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,14 +2,17 @@
 
 char *create_array(unsigned int size, char c)
 {
+    char *array;
+
     if (size == 0)
         return NULL;
 
-    char *array = malloc(size * sizeof(char));
+    array = malloc(size * sizeof(char));
     if (array == NULL)
         return NULL;
 
-    for (unsigned int i = 0; i < size; ++i)
+    unsigned int i;
+    for (i = 0; i < size; ++i)
     {
         array[i] = c;
     }
