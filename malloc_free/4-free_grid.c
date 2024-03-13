acc4a4c4c546c74a8113c83112fe3896d3e51e16
rayen445 <nesstinebacc@gmail.com>
2024-03-13 02:52:58 +0100

    commit

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,25 +1,12 @@
 #include <stdlib.h>
-#include "main.h"
 
 void free_grid(int **grid, int height)
 {
     int i;
-    
+    /* Free each row first */
     for (i = 0; i < height; i++) {
         free(grid[i]);
     }
-    
-    free(grid);
-}
-#include <stdlib.h>
-
-void free_grid(int **grid, int height)
-{
-    int i;
-    
-    for (i = 0; i < height; i++) {
-        free(grid[i]);
-    }
-    
+    /* Free the entire grid */
     free(grid);
 }
