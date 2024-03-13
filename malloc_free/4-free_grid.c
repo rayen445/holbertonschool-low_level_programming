769cc7e10bab0a8c4aec76147e1637efa85b4695
rayen445 <nesstinebacc@gmail.com>
2024-03-13 02:51:54 +0100

    commit

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,27 +1,25 @@
 #include <stdlib.h>
+#include "main.h"
 
-/**
- * free_grid - Frees a 2D grid created by alloc_grid
- * @grid: The 2D grid to be freed
- * @height: The height of the grid
- *
- * Return: None
- */
 void free_grid(int **grid, int height)
 {
-    if (grid == NULL || height <= 0)
-        return;
-
-    /* Free each row of the grid */
-    for (int i = 0; i < height; i++)
-    {
-        if (grid[i] != NULL)
-        {
-            free(grid[i]);
-            grid[i] = NULL;
-        }
+    int i;
+    
+    for (i = 0; i < height; i++) {
+        free(grid[i]);
     }
+    
+    free(grid);
+}
+#include <stdlib.h>
 
-    /* Free the grid itself */
+void free_grid(int **grid, int height)
+{
+    int i;
+    
+    for (i = 0; i < height; i++) {
+        free(grid[i]);
+    }
+    
     free(grid);
 }
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *str_concat(char *s1, char *s2);
+void print_grid(int **grid, int width, int height);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 
