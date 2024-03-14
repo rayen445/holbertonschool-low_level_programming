5bcc623ecf0e94ef6b7f85971a9247f64e3ac4f8
rayen445 <nesstinebacc@gmail.com>
2024-03-14 16:22:51 +0100

    commit

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a vriable of type struct dog 
+ * @: Pointer to the struct dog vriable to initialize
+ * @name: Pointer to the string containin the name of dog
+ * @age: age of the dog
+ * @owner: Pointer to the string containing the owner's name
+ */
+void init_dog(struct dog *d, char *name, float *age, char *owner) 
+{
+    if (d !=NULL)
+    {
+        d->name = name;
+        d->age = age
+        d->owner = owner
+    }
+}
