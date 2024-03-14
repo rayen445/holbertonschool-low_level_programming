e9d83b86bc298636930f533901885c6265ad675f
rayen445 <nesstinebacc@gmail.com>
2024-03-14 16:32:02 +0100

    commit

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,12 +8,12 @@
  * @age: age of the dog
  * @owner: Pointer to the string containing the owner's name
  */
-void init_dog(struct dog *d, char *name, float *age, char *owner) 
+void init_dog(struct dog *d, char *name, float age, char *owner) 
 {
     if (d !=NULL)
     {
         d->name = name;
-        d->age = age
-        d->owner = owner
+        d->age = age;
+        d->owner = owner;
     }
 }
