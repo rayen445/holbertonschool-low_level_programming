6f46f0bd9c7ad568fad4242f3d095bd0a4279df1
rayen445 <nesstinebacc@gmail.com>
2024-03-15 17:01:58 +0100

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -27,7 +27,7 @@ dog_t *new_dog (char *name, float age, char *owner)
         free (new_dog);
         return NULL;
     }
-    new_dog->owner = malloc(strlen(name) + 1)
+    new_dog->owner = malloc(strlen(name) + 1);
     if (new_dog->owner == NULL)
     {
         free (new_dog->name);
