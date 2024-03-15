e832ba2ff6370c9aed2879de04f384ff2aa9a63b
rayen445 <nesstinebacc@gmail.com>
2024-03-15 15:35:12 +0100

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * 
+ * Description: structure containing information about a dog 
+*/
 struct dog {
     char *name;
     float age ;
