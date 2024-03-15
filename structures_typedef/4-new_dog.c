6f406b7f5dbec972b48db5ae50cd5b6607ec2292
rayen445 <nesstinebacc@gmail.com>
2024-03-15 17:04:39 +0100

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,49 +4,51 @@
 #include "dog.h"
 
 /**
- * new dog - creates a new dog
- * @name: name of the dog 
- * @Age: Age of the dog
- * @owner: of the dog
- * 
- * Returne: pointer to the newly created dog, or NULL if failed
-*/
-dog_t *new_dog (char *name, float age, char *owner)
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: If memory allocation fails, return NULL.
+ *         Otherwise, return a pointer to the new dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *new_dog;
+    int name_len, owner_len;
 
     /* Allocate memory for the dog structure */
     new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
-    return NULL;
+        return (NULL);
 
-    /* Allocate memory for the name and owner string */
-    new_dog->name = malloc(strlen(name) + 1);
+    /* Get the length of the name and owner strings */
+    name_len = strlen(name);
+    owner_len = strlen(owner);
+
+    /* Allocate memory for the name string */
+    new_dog->name = malloc(name_len + 1);
     if (new_dog->name == NULL)
     {
-        free (new_dog);
-        return NULL;
+        free(new_dog);
+        return (NULL);
     }
-    new_dog->owner = malloc(strlen(name) + 1);
+
+    /* Allocate memory for the owner string */
+    new_dog->owner = malloc(owner_len + 1);
     if (new_dog->owner == NULL)
     {
-        free (new_dog->name);
-        return NULL;
-    }   
-    /*copy the name and owner string*/
-    strcpy (new_dog->name, name);
-    strcpy (new_dog->owner, owner);
+        free(new_dog->name);
+        free(new_dog);
+        return (NULL);
+    }
 
-    /*set the age*/
+    /* Copy the name and owner strings */
+    strcpy(new_dog->name, name);
+    strcpy(new_dog->owner, owner);
+
+    /* Set the age of the dog */
     new_dog->age = age;
-    
-    
-    
-    
-    
-    
-    return new_dog;
 
-
-
+    return (new_dog);
 }
