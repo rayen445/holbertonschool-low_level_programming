e9b779477e3f289b8b610535eb1e534d2010f137
rayen445 <nesstinebacc@gmail.com>
2024-03-15 17:06:29 +0100

    commti

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,54 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stdio.h>
 
-/**
- * new_dog - Creates a new dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Return: If memory allocation fails, return NULL.
- *         Otherwise, return a pointer to the new dog.
- */
-dog_t *new_dog(char *name, float age, char *owner)
+int main(void)
 {
-    dog_t *new_dog;
-    int name_len, owner_len;
+    dog_t *my_dog;
 
-    /* Allocate memory for the dog structure */
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-
-    /* Get the length of the name and owner strings */
-    name_len = strlen(name);
-    owner_len = strlen(owner);
-
-    /* Allocate memory for the name string */
-    new_dog->name = malloc(name_len + 1);
-    if (new_dog->name == NULL)
+    my_dog = new_dog("Ghost", 4.75, "Jon Snow");
+    if (my_dog == NULL)
     {
-        free(new_dog);
-        return (NULL);
+        printf("Failed to create a new dog\n");
+        return 1;
     }
 
-    /* Allocate memory for the owner string */
-    new_dog->owner = malloc(owner_len + 1);
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
-
-    /* Copy the name and owner strings */
-    strcpy(new_dog->name, name);
-    strcpy(new_dog->owner, owner);
-
-    /* Set the age of the dog */
-    new_dog->age = age;
-
-    return (new_dog);
+    printf("My dog's name is %s, and he is %.2f years old\n", my_dog->name, my_dog->age);
+    return 0;
 }
