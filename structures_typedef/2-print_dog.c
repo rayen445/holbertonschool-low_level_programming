ae9f9ca3dc1fbed317ad53a74dec8f83da2e90e6
rayen445 <nesstinebacc@gmail.com>
2024-03-15 14:30:52 +0100

    commit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 {
     if (d !=NULL)
     {
-        printf("name: %s\n", d->name !=NULL ? d->name : "(nil)");
-        printf("age: %.6f\n", d->age);
-        printf("owner: %s\n", d-> owner !=NULL ? d->owner : "(nil)");
+        printf("Name: %s\n", d->name !=NULL ? d->name : "(nil)");
+        printf("Age: %.6f\n", d->age);
+        printf("Owner: %s\n", d-> owner !=NULL ? d->owner : "(nil)");
     }
 
 
