dc183b418201aba98427ea5131e71a5e1b4d132a
rayen445 <nesstinebacc@gmail.com>
2024-03-15 14:25:22 +0100

    commit

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/*
+* print dog - prints a struct dog
+* @d pointer to the struct dog veriable to print
+*
+* description: this function print the contents of a struct dog veriable.
+*              if any element of the sturct is NULL, it print (nil) instead.
+*/
+viod print_dog(struct dog *d)
+{
+    if (d !=NULL)
+    {
+        printf("name: %s\n", d->name !=NULL ? d->name : "(nil)");
+        printf("age: %.6f\n", d->age);
+        printf("owner: %s\n", d-> owner !=NULL ? d->owner : "(nil)");
+    }
+
+
+
+}
