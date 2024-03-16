90bb9fcab2884515ba5a8d794d511c4dbfa47cec
rayen445 <nesstinebacc@gmail.com>
2024-03-16 17:59:51 +0100

    commit

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - ALLocated memory using malloc 
- * @b: the amount of memory to allocate
- * 
- * Return: A pointer to the alloucated memory
-*/
+ * malloc_checked - Allocates memory using malloc
+ * @b: The amount of memory to allocate
+ *
+ * Return: A pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc (b);
+    void *ptr = malloc(b);
+
     if (ptr == NULL)
     {
         exit(98);
