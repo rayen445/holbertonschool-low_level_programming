273e96170a2f77140d7a3f56c779349501a7d9c0
rayen445 <nesstinebacc@gmail.com>
2024-03-17 02:56:50 +0100

    commit

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,40 +13,41 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
     unsigned int s1_len = 0, s2_len = 0, i, j;
     char *concat;
+  
 
-    /* If s1 is NULL, treat it as an empty string */
+
+    
     if (s1 == NULL)
         s1 = "";
-    
-    /* If s2 is NULL, treat it as an empty string */
+ 
     if (s2 == NULL)
         s2 = "";
 
-    /* Calculate the length of s1 */
     while (s1[s1_len])
         s1_len++;
 
-    /* Calculate the length of s2 */
     while (s2[s2_len])
         s2_len++;
 
-    /* Allocate memory for the concatenated string */
     concat = malloc(sizeof(char) * (s1_len + n + 1));
 
-    /* Check if allocation failed */
+   
     if (concat == NULL)
         return NULL;
 
-    /* Copy characters from s1 to concat */
+    
     for (i = 0; i < s1_len; i++)
         concat[i] = s1[i];
 
-    /* Concatenate characters from s2 up to n or its entire length if n is greater than s2's length */
+    
     for (j = 0; j < n && j < s2_len; j++)
         concat[i++] = s2[j];
 
-    /* Null-terminate the concatenated string */
+    
     concat[i] = '\0';
+
+
+
 
     return concat;
 }
--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -14,11 +14,7 @@
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+         return
 }
 void free_dog(dog_t *d)
 {
