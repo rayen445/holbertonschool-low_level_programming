338aa75ce8831cdcd3023d7c08751d3d0dc0d801
rayen445 <nesstinebacc@gmail.com>
2024-03-17 02:14:51 +0100

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
-#include <stdlib.h> // For malloc
-#include <string.h> // For strlen, strcpy
+#include <stdlib.h> /* For malloc */
+#include <string.h> /* For strlen, strcpy */
 
 /**
  * new_dog - Creates a new dog with specified attributes
@@ -12,28 +12,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    // Check if name or owner is NULL
-    if (name == NULL || owner == NULL)
-        return NULL;
-
-    // Calculate the lengths of name and owner
-    size_t name_len = strlen(name);
-    size_t owner_len = strlen(owner);
-
-    // Declare variables
+    size_t name_len, owner_len;
     dog_t *new_dog;
     char *new_name, *new_owner;
 
-    // Allocate memory for the dog structure
+    /* Check if name or owner is NULL */
+    if (name == NULL || owner == NULL)
+        return NULL;
+
+    /* Calculate the lengths of name and owner */
+    name_len = strlen(name);
+    owner_len = strlen(owner);
+
+    /* Allocate memory for the dog structure */
     new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
         return NULL;
 
-    // Allocate memory for the name and owner strings
+    /* Allocate memory for the name and owner strings */
     new_name = malloc(sizeof(char) * (name_len + 1));
     new_owner = malloc(sizeof(char) * (owner_len + 1));
 
-    // Check if allocation failed
+    /* Check if allocation failed */
     if (new_name == NULL || new_owner == NULL) {
         free(new_name);
         free(new_owner);
@@ -41,7 +41,7 @@ dog_t *new_dog(char *name, float age, char *owner)
         return NULL;
     }
 
-    // Copy the name and owner strings
+    /* Copy the name and owner strings */
     strcpy(new_name, name);
     strcpy(new_owner, owner);
 
