60942496731a95fd8cea32616b66e3f5c4867d2c
rayen445 <nesstinebacc@gmail.com>
2024-03-17 02:53:15 +0100

    commit

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,11 +2,24 @@
 #include <stdlib.h>
 
 /**
- * free_dog  Frees memory allocated for a dog structure
+ * free_dog -#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees memory allocated for a dog structure
  * @d: Pointer to the dog structure to free
  *
  * Return: void
  */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
