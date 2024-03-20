bc7d8752187b8b7c734516819ab89d9b8268eaaa
rayen445 <nesstinebacc@gmail.com>
2024-03-20 20:56:01 +0100

    commit

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
-void array_iterator(int array, size_t, size void (*action) (int))
+void array_iterator(int array, size_t, size_t void (*action) (int))
 {
     if (array != NULL && action != NULL){
         for(size_t i=0; i <size; i++) {
