0ed83a14265bfd84d5ee0909c6daf80e0a3a8f8b
rayen445 <nesstinebacc@gmail.com>
2024-03-21 19:59:31 +0100

    commit

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -8,8 +8,8 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-        action(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
