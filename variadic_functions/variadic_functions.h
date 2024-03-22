bcd6cda86e74037053311574d4ae46ed1f8260b7
rayen445 <nesstinebacc@gmail.com>
2024-03-22 18:14:04 +0100

    commit

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,9 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/* Include necessary headers */
 #include <stdarg.h>
 
-/* Function prototype */
-int sum_them_all(const unsigned int n, ...);
+/* Function prototypes */
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
