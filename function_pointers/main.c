4fd923687b09d3ac4970c8b1a0e59e4774eff521
rayen445 <nesstinebacc@gmail.com>
2024-03-22 01:41:55 +0100

    commit

--- function_pointers/main.c
+++ function_pointers/main.c
@@ -1,65 +1,25 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
-/* Function prototypes */
-int is_98(int elem);
-int is_strictly_positive(int elem);
-int abs_is_98(int elem);
-
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * int_index - Function integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: Always zero.
  */
-int main(void)
+
+int int_index(int *array, int size, int (*cmp)(int))
 {
-    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-    int index;
+	int i;
 
-    /* Test with is_98 function */
-    printf("Using is_98 function:\n");
-    index = int_index(array, 20, is_98);
-    printf("Index of first occurrence of 98: %d\n", index);
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
 
-    /* Test with abs_is_98 function */
-    printf("\nUsing abs_is_98 function:\n");
-    index = int_index(array, 20, abs_is_98);
-    printf("Index of first occurrence of abs(98): %d\n", index);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
 
-    /* Test with is_strictly_positive function */
-    printf("\nUsing is_strictly_positive function:\n");
-    index = int_index(array, 20, is_strictly_positive);
-    printf("Index of first strictly positive number: %d\n", index);
-
-    return (0);
-}
-
-/**
- * is_98 - Checks if a number is equal to 98
- * @elem: The integer to check
- * Return: 1 if true, 0 otherwise
- */
-int is_98(int elem)
-{
-    return (98 == elem);
-}
-
-/**
- * is_strictly_positive - Checks if a number is strictly positive
- * @elem: The integer to check
- * Return: 1 if true, 0 otherwise
- */
-int is_strictly_positive(int elem)
-{
-    return (elem > 0);
-}
-
-/**
- * abs_is_98 - Checks if the absolute value of a number is 98
- * @elem: The integer to check
- * Return: 1 if true, 0 otherwise
- */
-int abs_is_98(int elem)
-{
-    return (elem == 98 || -elem == 98);
+	return (-1);
 }
