48c7765e83d269148352949a3c29f8b15cef8c1b
rayen445 <nesstinebacc@gmail.com>
2024-03-22 06:10:24 +0100

    commit

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stddef.h>
 
 /**
  * get_op_func - selects the correct function to perform the operation
@@ -9,22 +8,22 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op)
-    {
-        if (*(ops[i].op) == *s && !(*(s + 1)))
-            return (ops[i].f);
-        i++;
-    }
+	while (ops[i].op)
+	{
+		if (*(ops[i].op) == *s && !(*(s + 1)))
+			return (ops[i].f);
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
