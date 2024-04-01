da119b2af3a3976a718059ec37be4de3c0984519
rayen445 <nesstinebacc@gmail.com>
2024-04-01 17:12:34 +0200

    commit

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h> /* For exit */
+#include <stdlib.h>
 
 /* Function prototype */
-void *malloc_checked(unsigned int b);
+char *create_array(unsigned int size, char c);
 
 #endif /* MAIN_H */
