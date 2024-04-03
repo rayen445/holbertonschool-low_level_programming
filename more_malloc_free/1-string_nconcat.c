9e261c066c444d4027d5c60430299edd77f64d5e
rayen445 <nesstinebacc@gmail.com>
2024-04-03 23:39:25 +0200

    commit

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,55 @@
-#include <stdlib.h> /* For malloc */
-#include <stdio.h>  /* For NULL definition */
+#include <stdlib.h>
+
+/**
+ * _strlen - Calculates the length of a string
+ * @s: The string to calculate the length of
+ *
+ * Return: The length of the string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
 
 /**
  * string_nconcat - Concatenates two strings
  * @s1: The first string
  * @s2: The second string
- * @n: The maximum number of bytes of s2 to concatenate
+ * @n: The number of bytes of s2 to concatenate
  *
  * Return: Pointer to the concatenated string, or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int s1_len = 0, s2_len = 0, i, j;
-    char *concat;
-  
+	char *concat;
+	unsigned int len1, len2, i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    
-    if (s1 == NULL)
-        s1 = "";
- 
-    if (s2 == NULL)
-        s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
-    while (s1[s1_len])
-        s1_len++;
+	if (n >= len2)
+		n = len2;
 
-    while (s2[s2_len])
-        s2_len++;
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
 
-    concat = malloc(sizeof(char) * (s1_len + n + 1));
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
 
-   
-    if (concat == NULL)
-        return NULL;
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
 
-    
-    for (i = 0; i < s1_len; i++)
-        concat[i] = s1[i];
+	concat[i + j] = '\0';
 
-    
-    for (j = 0; j < n && j < s2_len; j++)
-        concat[i++] = s2[j];
-
-    
-    concat[i] = '\0';
-
-
-
-
-    return concat;
+	return (concat);
 }
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototypes */
-int _putchar(char c);
+unsigned int _strlen(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif /* MAIN_H */
