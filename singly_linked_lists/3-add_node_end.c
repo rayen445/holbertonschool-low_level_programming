655c89ef3b903e46e42c31ddaa5e6fe379106df3
rayen445 <nesstinebacc@gmail.com>
2024-04-03 23:25:04 +0200

    commit

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,46 +11,46 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node;
-    list_t *last = *head;
-    unsigned int len = 0;
+	list_t *new_node;
+	list_t *last = *head;
+	unsigned int len = 0;
 
-    /* Calculate the length of the string */
-    while (str[len])
-        len++;
+	/* Calculate the length of the string */
+	while (str[len])
+		len++;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Duplicate the string */
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	/* Duplicate the string */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    /* Set the length of the string */
-    new_node->len = len;
+	/* Set the length of the string */
+	new_node->len = len;
 
-    /* Since the new node will be added at the end, set its next to NULL */
-    new_node->next = NULL;
+	/* Since the new node will be added at the end, set its next to NULL */
+	new_node->next = NULL;
 
-    /* If the list is empty, make the new node as the head */
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	/* If the list is empty, make the new node as the head */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    /* Traverse the list to find the last node */
-    while (last->next != NULL)
-        last = last->next;
+	/* Traverse the list to find the last node */
+	while (last->next != NULL)
+		last = last->next;
 
-    /* Append the new node after the last node */
-    last->next = new_node;
+	/* Append the new node after the last node */
+	last->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
