ff973f74a903f43239c8605d1620a4e69c05ee87
rayen445 <nesstinebacc@gmail.com>
2024-04-05 11:03:15 +0200

    commit

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,7 +4,11 @@
 #include <stddef.h>
 
 int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
+
+int get_bit(unsigned long int n, unsigned int index);
+
 void print_binary(unsigned long int n);
+
+unsigned int binary_to_uint(const char *b);
 
 #endif /* MAIN_H */
