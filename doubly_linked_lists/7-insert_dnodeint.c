81282009851c64048c07b8c911140bdba667e927
rayen445 <nesstinebacc@gmail.com>
2024-04-05 09:47:32 +0200

    commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,43 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *temp = *h;
-    unsigned int i;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i;
 
-    if (!h) /* If h is NULL */
-        return (NULL);
+	if (!h) /* If h is NULL */
+		return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node) /* If malloc fails */
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node) /* If malloc fails */
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-    if (idx == 0) /* Insert at the beginning */
-    {
-        new_node->next = *h;
-        if (*h)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return (new_node);
-    }
+	if (idx == 0) /* Insert at the beginning */
+	{
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
-    for (i = 0; i < idx - 1 && temp != NULL; i++)
-        temp = temp->next;
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
 
-    if (temp == NULL) /* If idx is out of range */
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (temp == NULL) /* If idx is out of range */
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->next = temp->next;
-    if (temp->next)
-        temp->next->prev = new_node;
-    temp->next = new_node;
-    new_node->prev = temp;
+	new_node->next = temp->next;
+	if (temp->next)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+	new_node->prev = temp;
 
-    return (new_node);
+	return (new_node);
 }
