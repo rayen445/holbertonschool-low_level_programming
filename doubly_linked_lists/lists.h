5d43529e5b4f2524c8685eab1eb3ab51148f04b3
rayen445 <nesstinebacc@gmail.com>
2024-04-05 09:27:28 +0200

    commit

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -21,8 +21,8 @@ typedef struct dlistint_s
 
 /* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
-size_t print_dlistint_backward(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
