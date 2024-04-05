b039ab76311f009b44872a277bb2b1f2c942623c
rayen445 <nesstinebacc@gmail.com>
2024-04-05 09:45:14 +0200

    commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,52 +2,54 @@
 
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position.
- * @h: Pointer to a pointer to the head of the list.
- * @idx: Index of the list where the new node should be added.
- * @n: Data to store in the new node.
+ * @h: Pointer to a pointer to the head of the list
+ * @idx: Index of the list where the new node should be added
+ * @n: Value to store in the new node
  *
- * Return: Address of the new node, or NULL if it failed.
+ * Return: The address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp;
-	unsigned int i;
+    dlistint_t *new_node, *temp = *h;
+    unsigned int i = 0;
 
-	if (h == NULL) /* Check if head is NULL */
-		return (NULL);
+    new_node = malloc(sizeof(dlistint_t));
+    if (!new_node)
+        return (NULL);
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL) /* Check if malloc fails */
-		return (NULL);
+    new_node->n = n;
 
-	new_node->n = n; /* Assign data to the new node */
-	new_node->next = NULL;
-	new_node->prev = NULL;
+    if (!*h && idx == 0)
+    {
+        new_node->prev = NULL;
+        new_node->next = NULL;
+        *h = new_node;
+        return (new_node);
+    }
 
-	if (idx == 0) /* Insert at the beginning of the list */
-	{
-		new_node->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
+    while (temp)
+    {
+        if (i == idx)
+        {
+            new_node->prev = temp->prev;
+            new_node->next = temp;
+            if (temp->prev)
+                temp->prev->next = new_node;
+            else
+                *h = new_node;
+            temp->prev = new_node;
+            return (new_node);
+        }
+        temp = temp->next;
+        i++;
+    }
 
-	temp = *h;
-	for (i = 0; temp != NULL && i < idx - 1; i++)
-		temp = temp->next;
+    if (i == idx)
+    {
+        free(new_node);
+        return (add_dnodeint_end(h, n));
+    }
 
-	if (temp == NULL) /* If idx is out of range */
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = temp->next;
-	if (temp->next != NULL)
-		temp->next->prev = new_node;
-	temp->next = new_node;
-	new_node->prev = temp;
-
-	return (new_node);
+    free(new_node);
+    return (NULL);
 }
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,14 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
-/* Doubly linked list node structure */
+/* Structure for a doubly linked list node */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+    int n;                      /* Integer data */
+    struct dlistint_s *prev;    /* Pointer to the previous node */
+    struct dlistint_s *next;    /* Pointer to the next node */
 } dlistint_t;
 
 /* Function prototypes */
