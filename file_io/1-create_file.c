81279ddaf2d10d580337d3150923cb926cbafda4
rayen445 <nesstinebacc@gmail.com>
2024-04-05 12:04:33 +0200

    commit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,36 +1,31 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file with specified content
- * @filename: The name of the file to create
- * @text_content: The content to write to the file
+ * create_file - Creates a file and writes content to it
+ * @filename: Name of the file to create
+ * @text_content: NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written;
-    mode_t mode = S_IRUSR | S_IWUSR; /* rw------- */
+	int fd, len, wstatus;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    /* Create the file or truncate if it exists */
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    /* If text_content is provided, write it to the file */
-    if (text_content != NULL)
-    {
-        bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		wstatus = write(fd, text_content, len);
+		if (wstatus == -1 || wstatus != len)
+			return (-1);
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
