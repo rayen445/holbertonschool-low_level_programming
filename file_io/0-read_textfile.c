cebeedd9b43545925ab8a49fdb04a5dbb2af2193
rayen445 <nesstinebacc@gmail.com>
2024-04-05 12:02:20 +0200

    commit

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,53 +3,49 @@
 /**
  * read_textfile - Reads a text file and prints it to the POSIX standard output
  * @filename: The name of the file to read
- * @letters: The number of letters to read and print
+ * @letters: The number of letters it should read and print
  *
  * Return: The actual number of letters it could read and print,
- *         0 if the file can not be opened or read, or if @filename is NULL
+ *         0 if the file cannot be opened or read, or if filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	int fd;
+	ssize_t num_read, num_written;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    /* Open the file */
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    /* Allocate memory for buffer */
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    /* Read from file */
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	num_read = read(fd, buf, letters);
+	if (num_read == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 
-    /* Write to standard output */
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	num_written = write(STDOUT_FILENO, buf, num_read);
+	if (num_written == -1 || num_written != num_read)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 
-    /* Clean up and return the number of bytes written */
-    free(buffer);
-    close(fd);
-    return (bytes_written);
+	close(fd);
+	free(buf);
+
+	return (num_written);
 }
