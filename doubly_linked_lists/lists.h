344adbf8c348a09c7c9ff3094f1dadb5f74f3b64
rayen445 <nesstinebacc@gmail.com>
2024-04-05 09:32:49 +0200

    commit

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,16 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  *
- * Description: doubly linked list node structure
+ * Description: Doubly linked list node structure
  */
 typedef struct dlistint_s
 {
@@ -21,9 +20,9 @@ typedef struct dlistint_s
 
 /* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 #endif /* LISTS_H */
