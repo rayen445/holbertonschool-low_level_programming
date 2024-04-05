ad4c1dae27e1206f6e05a886d568d7c6cf379f38
rayen445 <nesstinebacc@gmail.com>
2024-04-05 10:59:10 +0200

    commit

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,13 +1,35 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number
- * @n: The number to print in binary
+ * print_binary - prints the binary representation of a number
+ * @n: number to print in binary
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-        print_binary(n >> 1);
+	unsigned long int mask = 1;
+	int shift_count = 0;
 
-    _putchar((n & 1) + '0');
+	mask <<= (sizeof(unsigned long int) * 8 - 1);
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while ((n & mask) == 0)
+	{
+		mask >>= 1;
+	}
+
+	while (mask)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+		mask >>= 1;
+	}
 }
