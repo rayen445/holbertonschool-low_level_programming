98641dc7a0900ba1e89dbcd8afe2648bf071a97d
rayen445 <nesstinebacc@gmail.com>
2024-04-05 12:06:03 +0200

    commit

--- file_io/main.h
+++ file_io/main.h
@@ -1,14 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <string.h>
 
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
